eef118eca173ce04954630e5388c8b446f0c5394
stabile12 <jestabilerocha@gmail.com>
2022-12-03 02:07:13 -0300

    'just now' removed

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -18,7 +18,6 @@ const Message = ({ message }) => {
     <div ref={ref} className={`message ${message.senderId !== currentUser.uid && 'owner'}`}>
       <div className="messageInfo">
         <img src={message.senderId !== currentUser.uid? currentUser.photoURL : data.user.photoURL} alt="" />
-        <span>just now</span>
       </div>
       <div className="messageContent">
         <p>{message.text}</p>
